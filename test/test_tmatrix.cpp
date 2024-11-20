2d5824795f8d27394b5782dcccbf7e0d4de4b43d
Ayato-ai <vanya.vladimir2016@yandex.ru>
2024-11-20 19:09:32 +0300

    Fixturs repair linux build

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -56,37 +56,15 @@ protected:
 		matrix_3[0][0] = v_0_;
 		matrix_3[0][1] = v_1_;
 		matrix_3[0][2] = v_2_;
-	}
-	void SetUpCopy() {
-		matrix = new TDynamicMatrix<T>(3);
-		matrix_copy = new TDynamicMatrix<T>(3);
-		matrix_copy_2 = new TDynamicMatrix<T>(2);
-		//  1 1 1
-		//  2 2 2
-		//  3 3 3
-
-		T* pMem_0 = new T [3] { 1, 1, 1};
-		T* pMem_1 = new T [3] { 2, 2, 2};
-		T* pMem_2 = new T [3] { 3, 3, 3};
-
-		TDynamicVector<T> v_0(pMem_0, 3);
-		TDynamicVector<T> v_1(pMem_1, 3);
-		TDynamicVector<T> v_2(pMem_2, 3);
-
-		delete[] pMem_0;
-		delete[] pMem_1;
-		delete[] pMem_2;
-
-		matrix[0][0] = v_0;
-		matrix[0][1] = v_1;
-		matrix[0][2] = v_2;
 
 		*matrix_copy = *matrix;
 	}
+
 	void SetUpNewLenght(int lenght) {
 		matrix = new TDynamicMatrix<T>(lenght);
 	}
-	void TearDownCopy() {
+
+	void TearDown() {
 		delete matrix;
 		delete matrix_copy;
 		delete vector;
@@ -94,6 +72,7 @@ protected:
 		delete matrix_3;
 		delete vector_1;
 	}
+
 	TDynamicMatrix<T>* matrix;
 	TDynamicMatrix<T>* matrix_copy;
 	TDynamicVector<T>* vector;
@@ -120,127 +99,127 @@ TYPED_TEST_P(TDynamicMatrix_Test, throws_when_create_matrix_with_negative_length
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_create_copied_matrix) {
-	ASSERT_NO_THROW(this->SetUpCopy());
+	ASSERT_NO_THROW(this->SetUp());
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, copied_matrix_is_equal_to_source_one) {
-	this->SetUpCopy();
-	EXPECT_EQ(*matrix, *matrix_copy);
+	this->SetUp();
+	EXPECT_EQ(*this->matrix, *this->matrix_copy);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, copied_matrix_has_its_own_memory) {
-	this->SetUpCopy();
-	EXPECT_NE(matrix, matrix_copy);
+	this->SetUp();
+	EXPECT_NE(this->matrix, this->matrix_copy);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_get_size) {
 	this->SetUp();
-	EXPECT_EQ(matrix->size(), 3);
+	EXPECT_EQ(this->matrix->size(), 3);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_set_and_get_element) {
 	this->SetUp();
-	EXPECT_EQ(this->matrix[0][1], *vector);
+	EXPECT_EQ(this->matrix[0][1], *this->vector);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, throws_when_set_element_with_negative_index) {
 	this->SetUp();
-	ASSERT_ANY_THROW(matrix->at(-2) = 0);
+	ASSERT_ANY_THROW(this->matrix->at(-2) = 0);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, throws_when_set_element_with_too_large_index) {
 	this->SetUp();
-	ASSERT_ANY_THROW(matrix->at(5) = 0);
+	ASSERT_ANY_THROW(this->matrix->at(5) = 0);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_assign_matrix_to_itself) {
 	this->SetUp();
-	ASSERT_NO_THROW(*matrix = *matrix);
+	ASSERT_NO_THROW(*this->matrix = *this->matrix);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_assign_matrices_of_equal_size) {
-	this->SetUpCopy();
-	EXPECT_EQ(*matrix_copy, *matrix);
+	this->SetUp();
+	EXPECT_EQ(*this->matrix_copy, *this->matrix);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, assign_operator_change_matrix_size) {
-	this->SetUpCopy();
-	*matrix_copy_2 = *matrix;
+	this->SetUp();
+	*this->matrix_copy_2 = *this->matrix;
 
-	EXPECT_EQ(matrix_copy_2->size(), 3);
+	EXPECT_EQ(this->matrix_copy_2->size(), 3);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_assign_matrices_of_different_size) {
-	this->SetUpCopy();
-	*matrix_copy_2 = *matrix;
+	this->SetUp();
+	*this->matrix_copy_2 = *this->matrix;
 
-	EXPECT_EQ(*matrix, *matrix_copy_2);
+	EXPECT_EQ(*this->matrix, *this->matrix_copy_2);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, compare_equal_matrices_return_true) {
-	this->SetUpCopy();
-	*matrix_copy_2 = *matrix;
+	this->SetUp();
+	*this->matrix_copy_2 = *this->matrix;
 
-	EXPECT_EQ(*matrix == *matrix_copy_2, 1);
+	EXPECT_EQ(*this->matrix == *this->matrix_copy_2, 1);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, compare_matrix_with_itself_return_true) {
-	this->SetUpCopy();
-	EXPECT_EQ(*matrix == *matrix, 1);
+	this->SetUp();
+	EXPECT_EQ(*this->matrix == *this->matrix, 1);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, matrices_with_different_size_are_not_equal) {
-	this->SetUpCopy();
-	EXPECT_EQ(*matrix != *matrix_copy_2, 1);
+	this->SetUp();
+	EXPECT_EQ(*this->matrix != *this->matrix_copy_2, 1);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_add_matrices_with_equal_size) {
 	this->SetUp();
-	EXPECT_EQ(*matrix + *matrix, *matrix_3);
+	EXPECT_EQ(*this->matrix + *this->matrix, *this->matrix_3);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, cant_add_matrices_with_not_equal_size) {
-	this->SetUpCopy();
-	ASSERT_ANY_THROW(*matrix + *matrix_copy_2);
+	this->SetUp();
+	ASSERT_ANY_THROW(*this->matrix + *this->matrix_copy_2);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_subtract_matrices_with_equal_size) {
 	this->SetUp();
-	EXPECT_EQ(*matrix_3 - *matrix, *matrix);
+	EXPECT_EQ(*this->matrix_3 - *this->matrix, *this->matrix);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, cant_subtract_matrixes_with_not_equal_size) {
-	this->SetUpCopy();
-	ASSERT_ANY_THROW(*matrix - *matrix_copy_2);
+	this->SetUp();
+	ASSERT_ANY_THROW(*this->matrix - *this->matrix_copy_2);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_multiplying_matrix_by_scalar) {
 	this->SetUp();
-	EXPECT_EQ(*matrix * 2, *matrix_3);
+	EXPECT_EQ(*this->matrix * 2, *this->matrix_3);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_multiplying_matrix_by_vector_with_correct_size) {
 	this->SetUp();
-	EXPECT_EQ(*matrix * *vector, *vector_1 * 6);
+	EXPECT_EQ(*this->matrix * *this->vector, *this->vector_1 * 6);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, cant_multiplying_matrix_by_vector_with_not_correct_size) {
 	this->SetUp();
-	ASSERT_ANY_THROW(*matrix_copy_2 * *vector);
+	ASSERT_ANY_THROW(*this->matrix_copy_2 * *this->vector);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, can_multiplying_matrix_by_matrix_with_correct_size) {
 	this->SetUp();
-	EXPECT_EQ(*matrix * *matrix, *matrix * 6);
+	EXPECT_EQ(*this->matrix * *this->matrix, *this->matrix * 6);
 }
 
 TYPED_TEST_P(TDynamicMatrix_Test, cant_multiplying_matrix_by_matrix_with_not_correct_size) {
 	this->SetUp();
-	ASSERT_ANY_THROW(*matrix * *matrix_copy_2);
+	ASSERT_ANY_THROW(*this->matrix * *this->matrix_copy_2);
 }
 
 
 
 REGISTER_TYPED_TEST_CASE_P(TDynamicMatrix_Test, can_create_matrix_with_positive_length, cant_create_too_large_matrix, throws_when_create_matrix_with_negative_length, can_create_copied_matrix, copied_matrix_is_equal_to_source_one, copied_matrix_has_its_own_memory, can_get_size, can_set_and_get_element, throws_when_set_element_with_negative_index, throws_when_set_element_with_too_large_index, can_assign_matrix_to_itself, can_assign_matrices_of_equal_size, assign_operator_change_matrix_size, can_assign_matrices_of_different_size, compare_equal_matrices_return_true, compare_matrix_with_itself_return_true, matrices_with_different_size_are_not_equal, can_add_matrices_with_equal_size, cant_add_matrices_with_not_equal_size, can_subtract_matrices_with_equal_size, cant_subtract_matrixes_with_not_equal_size, can_multiplying_matrix_by_scalar, can_multiplying_matrix_by_vector_with_correct_size, cant_multiplying_matrix_by_vector_with_not_correct_size, can_multiplying_matrix_by_matrix_with_correct_size, cant_multiplying_matrix_by_matrix_with_not_correct_size);
-typedef ::testing::Types<int, double, long long, char, float, long, unsigned int, unsigned long, unsigned long long> ContainerTypes;
+typedef ::testing::Types<int, double, long long, char, float, long> ContainerTypes;
 INSTANTIATE_TYPED_TEST_CASE_P(ContainerTypesInstantiation, TDynamicMatrix_Test, ContainerTypes);
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -6,7 +6,7 @@ template <typename T>
 class TDynamicVector_Test : public ::testing::Test {
 protected:
 	void SetUp() {
-		vector = new TDynamicVector<T>(5);
+		vector_ = new TDynamicVector<T>(5);
 	}
 	void SetUpCopy() {
 		T* pMem = new T[3]{ 2, 5, 2 };
@@ -37,7 +37,7 @@ protected:
 		vector = new TDynamicVector<T>(lenght);
 	}
 	void TearDown() {
-		delete vector;
+		delete vector_;
 	}
 	void TearDownCopy() {
 		delete vector;
@@ -47,6 +47,7 @@ protected:
 		delete vector_4;
 		delete vector_copy;
 	}
+	TDynamicVector<T>* vector_;
 	TDynamicVector<T>* vector;
 	TDynamicVector<T>* vector_1;
 	TDynamicVector<T>* vector_2;
@@ -74,115 +75,132 @@ TYPED_TEST_P(TDynamicVector_Test, can_create_copied_vector) {
 
 TYPED_TEST_P(TDynamicVector_Test, copied_vector_is_equal_to_source_one) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector, *vector_copy);
+	EXPECT_EQ(*this->vector, *this->vector_copy);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, copied_vector_has_its_own_memory) {
 	this->SetUpCopy();
-	EXPECT_NE(vector, vector_copy);
+	EXPECT_NE(this->vector, this->vector_copy);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_get_size) {
-	EXPECT_EQ(5, vector->size());
+	EXPECT_EQ(5, this->vector_->size());
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_set_and_get_element) {
-	this->vector[0][0] = 2;
+	this->vector_[0][0] = 2;
 
-	EXPECT_EQ(2, this->vector[0][0]);
+	EXPECT_EQ(2, this->vector_[0][0]);
 }
 
 TYPED_TEST_P(TDynamicVector_Test, throws_when_set_element_with_negative_index) {
-	ASSERT_ANY_THROW(vector->at(-2) = 0);
+	ASSERT_ANY_THROW(this->vector_->at(-2) = 0);
 }
 
 TYPED_TEST_P(TDynamicVector_Test, throws_when_set_element_with_too_large_index) {
-	ASSERT_ANY_THROW(vector->at(8) = 0);
+	ASSERT_ANY_THROW(this->vector_->at(8) = 0);
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_assign_vector_to_itself) {
-	EXPECT_EQ(*vector, *vector);
+	EXPECT_EQ(*this->vector_, *this->vector_);
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_assign_vectors_of_equal_size) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector, *vector_copy);
+	EXPECT_EQ(*this->vector, *this->vector_copy);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, assign_operator_change_vector_size) {
 	this->SetUpCopy();
-	*vector = *vector_1;
-	EXPECT_EQ(vector->size(), vector_1->size());
+	*this->vector = *this->vector_1;
+	EXPECT_EQ(this->vector->size(), this->vector_1->size());
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_assign_vectors_of_different_size) {
 	this->SetUpCopy();
-	*vector = *vector_1;
-	EXPECT_EQ(*vector, *vector_1);
+	*this->vector = *this->vector_1;
+	EXPECT_EQ(*this->vector, *this->vector_1);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, compare_equal_vectors_return_true) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector == *vector_copy, 1);
+	EXPECT_EQ(*this->vector == *this->vector_copy, 1);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, compare_vector_with_itself_return_true) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector == *vector, 1);
+	EXPECT_EQ(*this->vector == *this->vector, 1);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, vectors_with_different_size_are_not_equal) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector != *vector_1, 1);
-	EXPECT_EQ(*vector == *vector_1, 0);
+	EXPECT_EQ(*this->vector != *this->vector_1, 1);
+	EXPECT_EQ(*this->vector == *this->vector_1, 0);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_add_scalar_to_vector) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector_2, *vector + 2);
+	EXPECT_EQ(*this->vector_2, *this->vector + 2);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_subtract_scalar_from_vector) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector, *vector_2 - 2);
+	EXPECT_EQ(*this->vector, *this->vector_2 - 2);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_multiply_scalar_by_vector) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector_3, *vector * 2);
+	EXPECT_EQ(*this->vector_3, *this->vector * 2);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_add_vectors_with_equal_size) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector_2 + *vector, *vector_4);
+	EXPECT_EQ(*this->vector_2 + *this->vector, *this->vector_4);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, cant_add_vectors_with_not_equal_size) {
 	this->SetUpCopy();
-	ASSERT_ANY_THROW(*vector_1 + *vector);
+	ASSERT_ANY_THROW(*this->vector_1 + *this->vector);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_subtract_vectors_with_equal_size) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector_4 - *vector, *vector_2);
+	EXPECT_EQ(*this->vector_4 - *this->vector, *this->vector_2);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, cant_subtract_vectors_with_not_equal_size) {
 	this->SetUpCopy();
-	ASSERT_ANY_THROW(*vector_1 - *vector);
+	ASSERT_ANY_THROW(*this->vector_1 - *this->vector);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, can_multiply_vectors_with_equal_size) {
 	this->SetUpCopy();
-	EXPECT_EQ(*vector_2 * *vector, 51);
+	EXPECT_EQ(*this->vector_2 * *this->vector, 51);
+	this->TearDownCopy();
 }
 
 TYPED_TEST_P(TDynamicVector_Test, cant_multiply_vectors_with_not_equal_size) {
 	this->SetUpCopy();
-	ASSERT_ANY_THROW(*vector_1 * *vector);
+	ASSERT_ANY_THROW(*this->vector_1 * *this->vector);
+	this->TearDownCopy();
 }
 
 
 REGISTER_TYPED_TEST_CASE_P(TDynamicVector_Test, can_create_vector_with_positive_length, cant_create_too_large_vector, throws_when_create_vector_with_negative_length, can_create_copied_vector, copied_vector_is_equal_to_source_one, copied_vector_has_its_own_memory, can_get_size, can_set_and_get_element, throws_when_set_element_with_negative_index, throws_when_set_element_with_too_large_index, can_assign_vector_to_itself, can_assign_vectors_of_equal_size, assign_operator_change_vector_size, can_assign_vectors_of_different_size, compare_equal_vectors_return_true, compare_vector_with_itself_return_true, vectors_with_different_size_are_not_equal, can_add_scalar_to_vector, can_subtract_scalar_from_vector, can_multiply_scalar_by_vector, can_add_vectors_with_equal_size, cant_add_vectors_with_not_equal_size, can_subtract_vectors_with_equal_size, cant_subtract_vectors_with_not_equal_size, can_multiply_vectors_with_equal_size, cant_multiply_vectors_with_not_equal_size);
-typedef ::testing::Types<int, double, long long, char, float, long, unsigned int, unsigned long, unsigned long long> ContainerTypes;
+typedef ::testing::Types<int, double, long long, char, float, long> ContainerTypes;
 INSTANTIATE_TYPED_TEST_CASE_P(ContainerTypesInstantiation, TDynamicVector_Test, ContainerTypes);
